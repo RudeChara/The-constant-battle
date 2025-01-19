9009ad82aac9bd55c4ebc3c3b00bdcd9e9e73f47
misha <m9339323029@gmail.com>
2025-01-19 20:06:46 +0700

    Dev 001

--- main.py
+++ main.py
@@ -1,1 +1,66 @@
-print(123)
+import pygame
+import os
+import sys
+
+
+def load_image(name, color_key=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error as message:
+        print('Не удаётся загрузить:', name)
+        raise SystemExit(message)
+    image = image.convert_alpha()
+    if color_key is not None:
+        if color_key is -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    return image
+
+
+pygame.init()
+size = width, height = 500, 500
+screen = pygame.display.set_mode(size)
+sprite_group = pygame.sprite.Group()
+hero_group = pygame.sprite.Group()
+tile_width = tile_height = 50
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():
+    fon = pygame.transform.scale(load_image('fon.jpg'), size)
+    screen.blit((fon), (0, 0))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                return
+        pygame.display.flip()
+
+
+
+if __name__ == '__main__':
+    pygame.display.set_caption('Марио')
+    player = None
+    ranning = True
+    start_screen()
+    while ranning:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                ranning = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    pass
+                if event.key == pygame.K_DOWN:
+                    pass
+                if event.key == pygame.K_RIGHT:
+                    pass
+                if event.key == pygame.K_LEFT:
+                    pass
+        screen.fill(pygame.Color('black'))
+        pygame.display.flip()
+    pygame.quit()
