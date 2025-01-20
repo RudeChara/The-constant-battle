5a5bec8d3b0de1ba6268995ad915f167d7623b00
itcube22 <104897682+itcube22@users.noreply.github.com>
2025-01-20 18:49:57 +0700

    Очередной мусор в наш проект

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import pygame
 import os
 import sys
+from pygame import Surface
+from pygame.sprite import Sprite
+#Это калл
+
+def button_down(screen, event_pos, sprite):
+    if sprite.sprites()[0].rect.x >= event_pos[0] and sprite.sprites()[0].rect.x[1] + 100 >= event_pos[0]:
+        print('1222')
 
 
 def load_image(name, color_key=None):
@@ -19,11 +26,33 @@ def load_image(name, color_key=None):
 
 
 pygame.init()
-size = width, height = 500, 500
-screen = pygame.display.set_mode(size)
+size = width, height = 1920, 1080
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 sprite_group = pygame.sprite.Group()
 hero_group = pygame.sprite.Group()
 tile_width = tile_height = 50
+
+
+def get_all_sprites(sprite: pygame.sprite.Sprite, pos, refors=False) -> pygame.sprite.Group:
+    # создадим группу, содержащую все спрайты
+    x, y = pos
+    all_sprites = pygame.sprite.Group()
+    # создадим 20 спрайтов
+    for _ in range(1):
+        # создадим спрайт и добавим его в группу
+        sprite: Sprite = pygame.sprite.Sprite(all_sprites)
+        # определим его вид
+        sprite.image = pygame.image.load("data/button.png")
+        sprite.image = pygame.transform.scale(sprite.image, (300, 300))
+        sprite.image = pygame.transform.flip(sprite.image, refors, False)
+        # и размеры
+        sprite.rect = sprite.image.get_rect()
+        # разместим спрайт
+
+        sprite.rect.x = x
+        sprite.rect.y = y
+    return all_sprites
+
 
 def terminate():
     pygame.quit()
@@ -33,6 +62,7 @@ def terminate():
 def start_screen():
     fon = pygame.transform.scale(load_image('fon.jpg'), size)
     screen.blit((fon), (0, 0))
+    get_all_sprites(screen, pos=(100, 100))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -42,11 +72,11 @@ def start_screen():
         pygame.display.flip()
 
 
-
 if __name__ == '__main__':
     pygame.display.set_caption('Марио')
     player = None
     ranning = True
+    all_sprites = get_all_sprites(screen, pos=(100, 100))
     start_screen()
     while ranning:
         for event in pygame.event.get():
@@ -61,6 +91,10 @@ if __name__ == '__main__':
                     pass
                 if event.key == pygame.K_LEFT:
                     pass
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                print(all_sprites.sprites()[0].rect.x)
+
         screen.fill(pygame.Color('black'))
+        all_sprites.draw(screen)
         pygame.display.flip()
     pygame.quit()
